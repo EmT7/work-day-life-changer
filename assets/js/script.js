5ae70a5e615441e03e65268b66edee48702fe14e
Miranda <ranileah7@gmail.com>
2021-12-07 19:17:25 -0800

    prevent default

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,21 @@
-$(document).ready(function(){
-    $('.saveBtn').on('click', function(){
-        const val = $(this).siblings('textarea').val()
-        const hour = $(this).siblings('textarea').siblings('.hours').val()
-        console.log(hour)
-        localStorage.setItem('', val)
-    })
-})
+//Set date
+const todayDisplayDate = $('#currentDay').text(moment().format("dddd, MMMM Do YYYY"));
+//Set time
+const currentTime = $("#currentTime").text(moment().format("LT"));
+
+
+
+
+//Save button function & storage
+$('.saveBtn').click(function(event){
+
+        event.preventDefault()
+        var input = $(this).siblings('textarea').val()
+        var hour = $(this).siblings('textarea').siblings('.hours').val()
+        localStorage.setItem('textarea', 'hours');
+)};
+
+
+
+
+   
